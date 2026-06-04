75f88bf24a3d18f1fc0e503d82a44dce088c2193
Christian G <110692306+Blu3Blu3@users.noreply.github.com>
2026-06-04 11:10:40 -0700

    Update W1S1.py
    Completed up to Problem 6, saving again

--- W1S1.py
+++ W1S1.py
@@ -35,10 +35,76 @@ def greeting(name):
   print(f"Welcome to the Hundred Acre Wood {name}! My name is Christopher Robin.")
 
 '''
+3. Catchphrase
+Write a function "print_catchphrase()" that accepts a string "character" as a parameter and prints their
+catchphrase as outlined here:
+"Pooh" -> "Oh bother!"
+"Tigger" -> "TTFN: Ta-ta for now!"
+"Eeyore" -> "Thanks for noticing me."
+"Christopher Robin" -> "Silly old bear."
+Anything else -> "Sorry! I don't know <character>'s catchphrase!"
+
+This is asking for specific strings to be printed based on what "character" is.
+Since there are specific cases to check "character" for, this requires either a switch statement or a series
+of conditional checks. I'll use a switch statement for better clarity.
 '''
+def print_catchphrase(character):
+  match character:
+    case "Pooh":
+      print("Oh bother!")
+    case "Tigger":
+      print("TTFN: Ta-ta for now!")
+    case "Eeyore":
+      print("Thanks for noticing me.")
+    case "Christopher Robin":
+      print("Silly old bear.")
+    case _:
+      print(f"Sorry! I don't know {character}'s catchphrase!")
 
+'''
+4. Return Item
+Implement a function "get_item()" that accepts a 0-indexed list items and a non-negative integer x, and
+returns the element at index x in items. If x is an invalid index, return None.
 
+This is simply asking to return items[x], or None if x is out of bounds.
+Python doesn't have a method that automatically returns None if a target index is OOB, so I'm adding a
+condition to check for that.
+'''
+def get_item(items, x):
+  if(x < len(items) and x >= 0):
+    return items[x]
+  else:
+    return None
 
+'''
+5. Total Honey
+Write a function "sum_honey()" that accepts a list of integers "hunny_jars" and returns the sum of all
+elements in the list. Do not use "sum()".
+
+Since "sum()" isn't allowed, this is asking us to iterate through the list "hunny_jars" and add up all the
+values with some catch variable. A for loop would be easiest to implement this in.
+'''
+def sum_honey(hunny_jars):
+  sum = 0
+  for h in hunny_jars:
+    sum += h
+  return sum
+
+'''
+6. Double Trouble
+Write a function "doubled()" that accepts a list of integers "hunny_jars" and multiplies each element in
+the list by two. Return the doubled list.
+
+This follows the same implementation as problem 5, only multiplying each element iterated over instead of
+adding it to a sum variable. However, multiplying the elements in the list given will carry over into the
+rest of the code unless a copy of the list is used instead. So, instead of multiplying the elements in
+"hunny_jars" directly, I'll copy it into another variable and multiply over that.
+'''
+def doubled(hunny_jars):
+  ret = hunny_jars.copy()
+  for h in range(len(ret)):
+    ret[h] *= 2
+  return ret
 
 
 
