cddde01fd84ae5f431512917e9409847eedbe21f
Christian G <110692306+Blu3Blu3@users.noreply.github.com>
2026-06-04 10:43:01 -0700

    Create W1S1.py
    First 2 problems done, just wanted to save

---
+++ W1S1.py
@@ -0,0 +1,48 @@
+'''
+Christian Guiang
+Week 1, Session 1
+Started: 6/4/2026
+Finished: -----
+Validated: -----
+Last updated: 6/4/2026
+
+Hi! These problems are going to be paraphrased from the course and answered in both the comments and code.
+Once all the problems are solved, code to test it will be added at the bottom of this file; from there,
+feel free to run this and validate that everything works.
+
+By the way, the comments will describe my thought process following the "Understand, Plan, Implement"
+framework "UPI", as practice for doing the same in a live interview.
+'''
+
+'''
+1. Hundred Acre Wood
+Write a function "welcome()" that prints the string "Welcome to the Hundred Acre Wood!"
+
+This is simply using "print()" inside a function. 
+'''
+def welcome():
+  print("Welcome to the Hundred Acre Wood!")
+
+'''
+2. Greeting
+Write a function "greeting()" that accepts a single parameter, a string "name", and prints:
+"Welcome to the Hundred Acre Wood <name>! My name is Christopher Robin."
+
+This is asking for a string to be added in the middle of another to create a greeting.
+To do this, either a formatted string "f-string" or concatenating pieces "Welcome..." + name + "! My name..."will work.
+'''
+def greeting(name):
+  print(f"Welcome to the Hundred Acre Wood {name}! My name is Christopher Robin.")
+
+'''
+'''
+
+
+
+
+
+
+
+
+
+
